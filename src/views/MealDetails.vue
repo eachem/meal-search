368c9c386663cf3444195949bf2707ceb8ec74b6
eachem <achem11298@gmail.com>
2024-04-27 02:11:14 -0500

    MealDetails is working

--- src/views/MealDetails.vue
+++ src/views/MealDetails.vue
@@ -2,15 +2,15 @@
 
 import { onMounted, ref } from 'vue';
 import useAPI from '@/composables/useAPI'
-import { useRoute, useRouter } from 'vue-router';
+import {useRoute} from 'vue-router'
 
 const { api } = useAPI()
 const route = useRoute()
 const meal = ref({})
 
 onMounted(() => {
-    api.get('lookup.php?i?i=${route.params.id}')
-        .then(({ data }) => {
+    api.get('lookup.php?i=${route.params.id}')
+        .then(({data}) => {
             meal.value = data.meals[0] || {}
         })
 })
@@ -18,8 +18,19 @@ onMounted(() => {
 </script>
 
 <template>
-    <div class="">
-        {{ meal }}
+    <div class="m-10">
+        {{ meal }} 
+        <h1 class="text-5xl font-bold mb-10">{{ meal.strMeal }}</h1>
+        <img :src="meal.strMealThumb" class="max-w-[30%] mb-10"/>
+        <p class="mb-10">{{ meal.strInstructions }}"</p>
+
+        <ul>
+            <li v-for="i in 20" :key="i">
+                <div v-if="meal['strIngredients' + i] !== ''">
+                    {{ i }}. {{ meal['strIngredients' + i] }}
+                </div>
+            </li>
+        </ul>
 
     </div>
 
