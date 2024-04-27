e4e1d3677fb15686080d4e5aaaacebbf375a9c4b
eachem <achem11298@gmail.com>
2024-04-27 01:23:24 -0500

    MealDetails working

--- src/router.js
+++ src/router.js
@@ -4,17 +4,18 @@ import HomePage from '@/views/HomePage.vue'
 import MealsByName from './views/MealsByName.vue'
 import MealsByLetter from './views/MealsByLetter.vue'
 import MealDetails from './views/MealDetails.vue'
-import MealByIngredients from './views/MealByIngredients.vue'
 import Ingredients from './views/Ingredients.vue'
+import MealByIngredients from './views/MealByIngredients.vue'
+
 
 const routes = [
   { path: '/meal-search/', name: 'Home', component: HomePage },
   { path: '/meal-search/by-name/:name?', name: 'MealsByName', component: MealsByName },
   { path: '/meal-search/by-letter/:letter?', name: 'MealsByLetter', component: MealsByLetter },
-  { path: '/meal-search/by-ingredient/:ingredient?', name: 'MealByIngredients', component: MealByIngredients },
+  { path: '/meal-search/by-ingredients/:ingredients?', name: 'MealByIngredients', component: MealByIngredients },
   { path: '/meal-search/:id', name: 'MealDetails', component: MealDetails },
   { path: '/meal-search/ingredients', name: 'Ingredients', component: Ingredients },
-  //{ path: '/other', name: 'Other', component: () => import('@/views/OtherPage.vue') },
+  { path: '/meal-search/other', name: 'Other', component: () => import('@/views/OtherPage.vue') },
 ]
 
 const router = createRouter({
--- src/views/MealDetails.vue
+++ src/views/MealDetails.vue
@@ -1,6 +1,26 @@
 <script setup>
+
+import { onMounted, ref } from 'vue';
+import useAPI from '@/composables/useAPI'
+import { useRoute, useRouter } from 'vue-router';
+
+const { api } = useAPI()
+const route = useRoute()
+const meal = ref({})
+
+onMounted(() => {
+    api.get('lookup.php?i?i=${route.params.id}')
+        .then(({ data }) => {
+            meal.value = data.meals[0] || {}
+        })
+})
+
 </script>
 
 <template>
-    
+    <div class="">
+        {{ meal }}
+
+    </div>
+
 </template>
